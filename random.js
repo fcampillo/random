cc00f7ce40717c638862df37162cdb67e60b8f6b
Fernando <f.campillo.calatayud@gmail.com>
2016-10-14 15:37:51 +0200

    random versión 2

--- random.js
+++ random.js
@@ -1,9 +1,10 @@
 // Math.random() devulve un número aleatorio entre 0 y 1.
 var numero = Math.random();
 
+var str = ' MAYOR que 0,5';
+
 if (numero <= 0.5) {
-  console.log('\n' + numero + ' MENOS que 0,5 \n');
+  str = ' MENOR que 0,5';
 }
-else {
-  console.log('\n' + numero + 'MAYOR que 0,5 \n');
-}
+
+console.log('\n' + numero + str + '\n');
